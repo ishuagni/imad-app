422c8172e9e08fb46232535228a23b42947f7103
Shaurya <ishuagnihotri20@gmail.com>
2018-04-03 11:26:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/', function (req, res) {
 
 app.get('/counter', function (req, res) {
   counter += 1;
-  res.send(count.toString());
+  res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
