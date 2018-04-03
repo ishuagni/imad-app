62a63a923aa20b90272908ad91a0e9bbaefb60ea
Shaurya <ishuagnihotri20@gmail.com>
2018-04-03 11:08:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
-var counter = document.getElementById("counter");
+var counter = document.getElementById('counter');
 var c = 0;
 counter.onclick = function() {
-    var count = document.getElementById("count");
+    var count = document.getElementById('count');
     c += 1;
     count.innerHtml = c.toString();
 };
