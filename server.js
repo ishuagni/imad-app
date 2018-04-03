7ac0fee9681b3a21bffa5700a2bfc2452d807790
Shaurya <ishuagnihotri20@gmail.com>
2018-04-04 00:51:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = requires('pg').Pool;
+
+var config = {
+    username : 'ishuagnihotri20',
+    database : 'ishuagnihotri20',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -55,6 +64,18 @@ function createTemplate (data)
     `;
     return htmlTemplate;
 }
+
+var pool = new Pool(config);
+app.get('/db-test', function(req, res){
+        pool.query('Select * from test;',function(err, result){
+            if(err){
+                res.status(500).send(err.toString());
+            }
+            else{
+                res.send(JSON.stringify(result));
+            }
+        });
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
