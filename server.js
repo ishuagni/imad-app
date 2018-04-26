aa8f6d4d692c16b9d933d577c6affb069bfff94c
Shaurya <ishuagnihotri20@gmail.com>
2018-04-26 17:14:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,10 +81,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-/*app.get('/:articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
   var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
-});*/
+});
 
 app.get('/counter', function (req, res) {
   counter += 1;
